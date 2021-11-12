3b1713376e5f8720444d11f381156e4a2ec995c6
iori0213 <iori19980213@gmail.com>
2021-11-12 17:15:54 +0800

    this is the MiCard font, icon part.

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,78 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'this is project mi card',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'mi_card',
-            textAlign: TextAlign.center,
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Bryan.jpg'),
+              ),
+              Text(
+                'Bryan',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 80.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.phone),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '0928 010 666',
+                      style: TextStyle(
+                        fontFamily: 'SansPro',
+                        fontSize: 15.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 80.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.email),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'bryan@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SansPro',
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-        backgroundColor: Colors.teal,
-        body: Container(),
       ),
     );
   }
